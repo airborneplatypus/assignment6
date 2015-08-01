86d684d5c923b4931fc72206ec652a39f516881d
Tyler Koistinen <koistinentyler@gmail.com>
2015-08-01 12:17:30 -0700

    Initial Commit

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "<html>\n";
+echo "<head>\n";
+echo "<title> Login </title>\n";
+echo "</head>\n";
+echo "<body>\n";
+
+
+echo "</body>\n";
+echo "</html>\n";
+
+?>
