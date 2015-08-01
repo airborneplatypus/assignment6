2a0e319aa79075625831b50f19b7f4fd0d031537
Tyler Koistinen <koistinentyler@gmail.com>
2015-08-01 12:46:34 -0700

    added db info

--- src/index.php
+++ src/index.php
@@ -3,6 +3,21 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+$dbhost = 'oniddb.cws.oregonstate.edu';
+$dbname = 'koistint-db';
+$dbuser = 'koistint-db';
+$dbpass = '48W7fqmF2hJOtdFA';
+
+$mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
+    or die("Error connecting to database server");
+
+mysql_select_db($dbname, $mysql_handle)
+    or die("Error selecting database: $dbname");
+
+echo 'Successfully connected to database!';
+
+mysql_close($mysql_handle);
+
 echo "<html>\n";
 echo "<head>\n";
 echo "<title> Login </title>\n";
